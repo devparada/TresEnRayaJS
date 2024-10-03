2c1aa7c043a5d9c214286d7bd09d1fa397296f34
Raúl <raul.parada040103@gmail.com>
2024-10-03 20:43:52 +0200

    Divido el script en dos scripts + correcciones

---
+++ js/comprobaciones.js
@@ -0,0 +1,59 @@
+/* Este script contiene las comprobaciones que se usa en este script y en el de game */
+function comprobarValido(celda) {
+    // \u00A0 -> es &nbsp; (un espacio)
+    if (document.getElementById(celda.id).getAttribute("value") == "\u00A0\u00A0\u00A0") {
+        return true;
+    }
+    return false;
+}
+
+function comprobarEstadoPartida(estado) {
+    if (estado) {
+        celdas.forEach((celda) => {
+            console.log(document.getElementById(celda.id));
+            if (document.getElementById(celda.id) == null) {
+                finPartida = true;
+                mostrarEmpate();
+            } else {
+                finPartida = false;
+            }
+        });
+    } else {
+        celdas.forEach((celda) => {
+            celda.setAttribute("disable", "");
+        });
+    }
+}
+
+function comprobarGanador() {
+    var combinacionesGanadoras = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    var comprobarFlag = false;
+
+    for (let i = 0; i < combinacionesGanadoras.length; i++) {
+        let primerNumero = celdas[combinacionesGanadoras[i][0]].children[0];
+        let segundoNumero = celdas[combinacionesGanadoras[i][1]].children[0];
+        let tercerNumero = celdas[combinacionesGanadoras[i][2]].children[0];
+
+        if (primerNumero.value == "X" && segundoNumero.value == "X" && tercerNumero.value == "X") {
+            colorearResultado(primerNumero, segundoNumero, tercerNumero, true);
+            mostrarResultado(true);
+            comprobarFlag = true;
+        } else if (primerNumero.value == "O" && segundoNumero.value == "O" && tercerNumero.value == "O") {
+            colorearResultado(primerNumero, segundoNumero, tercerNumero, false);
+            mostrarResultado(false);
+            comprobarFlag = true;
+        }
+    }
+
+    finPartida = comprobarFlag;
+    return comprobarFlag;
+}
--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
-// Variables del script
+// Constantes
 const celdas = document.querySelectorAll("td");
 const elemResultado = document.getElementById("resultado");
+
+// Variables
 var finPartida = false;
 var click = 0;
 
@@ -46,12 +48,6 @@ function segundoJugadorCPU() {
     }
 }
 
-function mostrarEmpate() {
-    if (finPartida) {
-        elemResultado.innerText = "Empate";
-    }
-}
-
 async function colorearResultado(celda0, celda1, celda2, estado) {
     if (!finPartida) {
         // Red
@@ -85,61 +81,8 @@ function mostrarResultado(estado) {
     }
 }
 
-function comprobarValido(celda) {
-    // \u00A0 -> es &nbsp; (un espacio)
-    if (document.getElementById(celda.id).getAttribute("value") == "\u00A0\u00A0\u00A0") {
-        return true;
-    }
-    return false;
-}
-
-function comprobarEstadoPartida(estado) {
-    if (estado) {
-        celdas.forEach((celda) => {
-            if (comprobarValido(celda)) {
-                finPartida = true;
-                mostrarEmpate();
-            } else {
-                finPartida = false;
-            }
-        });
-    } else {
-        celdas.forEach((celda) => {
-            celda.setAttribute("disable", "");
-        });
-    }
-}
-
-function comprobarGanador() {
-    var combinacionesGanadoras = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-
-    for (let i = 0; i < combinacionesGanadoras.length; i++) {
-        let primerNumero = celdas[combinacionesGanadoras[i][0]].children[0];
-        let segundoNumero = celdas[combinacionesGanadoras[i][1]].children[0];
-        let tercerNumero = celdas[combinacionesGanadoras[i][2]].children[0];
-
-        if (primerNumero.value == "X" && segundoNumero.value == "X" && tercerNumero.value == "X") {
-            colorearResultado(primerNumero, segundoNumero, tercerNumero, true);
-            mostrarResultado(true);
-            return true;
-        } else if (primerNumero.value == "O" && segundoNumero.value == "O" && tercerNumero.value == "O") {
-            colorearResultado(primerNumero, segundoNumero, tercerNumero, false);
-            mostrarResultado(false);
-            return true;
-        }
-    }
-
-    mostrarEmpate();
-    return false;
+function mostrarEmpate() {
+    elemResultado.innerText = "Empate";
 }
 
 function volverJugar() {
