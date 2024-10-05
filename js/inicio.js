560c8c8213f8c0119c555eb54dea6ff8128f00a6
Raúl <raul.parada040103@gmail.com>
2024-10-05 16:30:25 +0200

    Modifico header páginas + creada página vs 2 jug.

--- js/inicio.js
+++ js/inicio.js
@@ -1,5 +1,10 @@
 const botonCPU = document.querySelector("#boton-cpu");
+const boton2Jugadores = document.querySelector("#boton-2jugadores");
 
 botonCPU.addEventListener("click", function () {
     window.location.href = "pages/cpu.html";
+})
+
+boton2Jugadores.addEventListener("click", function () {
+    window.location.href = "pages/2jugadores.html";
 })
